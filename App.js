c11396628992f967e3da5a04f6bedbdb99b96211
Baswaraj1 <b.walke@rabbitandtortoise.com>
2020-08-28 11:42:53 +0530

    CI/CD commit by Baswaraj

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App  = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+  
+        
+          <View style={styles.body}>
+    
+         <Text>Check CI/CD</Text>
+         
+        
+          
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+});
+
+export default App;
